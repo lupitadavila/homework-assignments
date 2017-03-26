6bd28e0a393eeea100d71098829267190722d94d
Lupita <lupita.dav@gmail.com>
2017-03-26 16:56:37 -0700

    Make changes to Main.jsx

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,13 +1,23 @@
 var React = require('react');
-// var Nav = require('Nav')
+import 'react-bootstrap';
 
 var Main = (props) => {
   return (
     <div>
-      <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
-          <p>Main.jsx rendered</p>
-          {props.children}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            AssignmentList
+          </div>
+          <div className="col-md-9">
+            <div className="row">
+              Navbar
+            </div>
+            <div className="row">
+              <p className="well">Main.jsx rendered</p>
+              {props.children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
